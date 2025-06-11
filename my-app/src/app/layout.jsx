9390d8330ddf948343b2d6d3bf87c35c4740699a
Vicente Quiroz <99621vicenteaqv@gmail.com>
2025-06-10 23:56:23 -0400

    feat: add SVG files for icons and implement layout structure with global styles

---
+++ my-app/src/app/layout.jsx
@@ -0,0 +1,31 @@
+
+import "./styles/globals.css";
+import MenuBar from "@/components/MenuBar";
+
+
+
+export const metadata = {
+  title: "Cuatro Cuatros",
+  description: "sistema de facturacion y gestion de clientes",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className="antialiased"
+      >
+        <div className="min-h-screen bg-[#F6f6f6]">
+          <div className="container mx-auto p-6">
+            <div className="mb-8">
+              <h1 className="text-3xl font-bold text-gray-900 mb-2">Cuatro Catros</h1>
+              <p className="text-gray-600">Sistema de Inventario y Facturacion</p>
+            </div>
+            <MenuBar />
+          </div>
+        </div>
+        
+      </body>
+    </html>
+  );
+}
